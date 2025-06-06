179e88136af173c6ab6c2959e19170a7657a8449
Tom Amollo <iantom96@gmail.com>
2025-06-06 22:44:25 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -20,18 +20,164 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
+    [
+  {
+    "baked_goods": [
+      {
+        "bakery_id": 1,
+        "created_at": "2023-11-03 16:11:18",
+        "id": 1,
+        "name": "Chocolate dipped donut",
+        "price": 2.75,
+        "updated_at": null
+      },
+      {
+        "bakery_id": 1,
+        "created_at": "2023-11-03 16:11:18",
+        "id": 2,
+        "name": "Apple-spice filled donut",
+        "price": 3.5,
+        "updated_at": null
+      }
+    ],
+    "created_at": "2023-11-03 16:11:18",
+    "id": 1,
+    "name": "Delightful donuts",
+    "updated_at": null
+  },
+  {
+    "baked_goods": [
+      {
+        "bakery_id": 2,
+        "created_at": "2023-11-03 16:11:18",
+        "id": 3,
+        "name": "Glazed honey cruller",
+        "price": 3.25,
+        "updated_at": null
+      },
+      {
+        "bakery_id": 2,
+        "created_at": "2023-11-03 16:11:18",
+        "id": 4,
+        "name": "Chocolate cruller",
+        "price": 100,
+        "updated_at": "2023-11-03 16:11:18"
+      }
+    ],
+    "created_at": "2023-11-03 16:11:18",
+    "id": 2,
+    "name": "Incredible crullers",
+    "updated_at": null
+  }
+]
     return ''
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
+    {
+  "baked_goods": [
+    {
+      "bakery_id": 1,
+      "created_at": "2023-11-03 16:11:18",
+      "id": 1,
+      "name": "Chocolate dipped donut",
+      "price": 2.75,
+      "updated_at": null
+    },
+    {
+      "bakery_id": 1,
+      "created_at": "2023-11-03 16:11:18",
+      "id": 2,
+      "name": "Apple-spice filled donut",
+      "price": 3.5,
+      "updated_at": null
+    }
+  ],
+  "created_at": "2023-11-03 16:11:18",
+  "id": 1,
+  "name": "Delightful donuts",
+  "updated_at": null
+}
     return ''
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
+    [
+  {
+    "bakery": {
+      "created_at": "2023-11-03 16:31:32",
+      "id": 1,
+      "name": "Delightful donuts",
+      "updated_at": null
+    },
+    "bakery_id": 1,
+    "created_at": "2023-11-03 16:31:32",
+    "id": 2,
+    "name": "Apple-spice filled donut",
+    "price": 3.5,
+    "updated_at": null
+  },
+  {
+    "bakery": {
+      "created_at": "2023-11-03 16:31:32",
+      "id": 2,
+      "name": "Incredible crullers",
+      "updated_at": null
+    },
+    "bakery_id": 2,
+    "created_at": "2023-11-03 16:31:32",
+    "id": 4,
+    "name": "Chocolate cruller",
+    "price": 3.4,
+    "updated_at": null
+  },
+  {
+    "bakery": {
+      "created_at": "2023-11-03 16:31:32",
+      "id": 2,
+      "name": "Incredible crullers",
+      "updated_at": null
+    },
+    "bakery_id": 2,
+    "created_at": "2023-11-03 16:31:32",
+    "id": 3,
+    "name": "Glazed honey cruller",
+    "price": 3.25,
+    "updated_at": null
+  },
+  {
+    "bakery": {
+      "created_at": "2023-11-03 16:31:32",
+      "id": 1,
+      "name": "Delightful donuts",
+      "updated_at": null
+    },
+    "bakery_id": 1,
+    "created_at": "2023-11-03 16:31:32",
+    "id": 1,
+    "name": "Chocolate dipped donut",
+    "price": 2.75,
+    "updated_at": null
+  }
+]
     return ''
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
+    {
+  "bakery": {
+    "created_at": "2023-11-03 16:16:21",
+    "id": 1,
+    "name": "Delightful donuts",
+    "updated_at": null
+  },
+  "bakery_id": 1,
+  "created_at": "2023-11-03 16:16:21",
+  "id": 2,
+  "name": "Apple-spice filled donut",
+  "price": 3.5,
+  "updated_at": null
+}
     return ''
 
 if __name__ == '__main__':
